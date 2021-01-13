9c221d939582a0013778f73da17b856ed6fc27d6
Hamilkar247 <adonnibal96@gmail.com>
2021-01-13 13:34:51 +0100

    [WO-52] uzywanie argparse i parametr logów oraz grubos gifa
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- slideshow.py
+++ slideshow.py
@@ -12,10 +12,26 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QMovie
 import time
 import sys
+import logging
+import argparse
+
+
+def def_params():
+    parser = argparse.ArgumentParser(
+            description="parametry do wyswietlenia"
+    )
+    parser.add_argument("-l", "--log", action='store_true', help="ustaw debug flage")
+    parser.add_argument("-g", "--grubosc", default=20, help="ustaw grubosc gifa")
+    args = parser.parse_args()
+    if args.log:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("Ten komunikat pokazuje sie tylko w trybie debug")
+        print("args:" + str(args))
+    return args
 
 class Ui_MainWindow(object):
 
-    def __init__(self):
+    def __init__(self, gruboscGifa):
         self.label = None #label na gifa
         self.label_2 = None #label na widget/mape
         self.movie = None #movie do odpalenia gifa
@@ -24,7 +40,7 @@ class Ui_MainWindow(object):
         self.centralWidget = None
         self.widthWindow = 925
         self.heightWindow = 810
-        self.gruboscGifa = 20
+        self.gruboscGifa = int(gruboscGifa)
         self.MainWindow = None
 
     def setupUi(self, MainWindow):
@@ -76,9 +92,9 @@ class Ui_MainWindow(object):
 
 class Window(QtWidgets.QMainWindow):
     resized = QtCore.pyqtSignal()
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent=parent)
-        self.ui = Ui_MainWindow()
+    def __init__(self, gruboscGifa):
+        super(Window, self).__init__(parent=None)
+        self.ui = Ui_MainWindow(gruboscGifa)
         self.ui.setupUi(self)
         self.resized.connect(self.resizeEventFunction)
 
@@ -91,7 +107,9 @@ class Window(QtWidgets.QMainWindow):
         print("resizeEvent")
 
 if __name__ == "__main__":
+    args=def_params()
+    gruboscGifa=args.grubosc
     app = QtWidgets.QApplication(sys.argv)
-    w = Window()
+    w = Window(gruboscGifa)
     w.show()
     sys.exit(app.exec_())
