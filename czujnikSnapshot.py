4cb4300c3860bc3f7dd066e0ddf9d0eefab58a99
Hamilkar247 <adonnibal96@gmail.com>
2021-01-22 13:18:34 +0100

    [WO-67] dostosowanie czujnikSnapshot.py do pobierania zdjec z json
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- czujnikSnapshot.py
+++ czujnikSnapshot.py
@@ -7,8 +7,9 @@ from selenium.webdriver.chrome.options import Options
 import os
 import json
 from time import sleep
-import datetime
+from datetime import datetime
 import sys
+import time
 
 def def_params():
     parser = argparse.ArgumentParser(
@@ -37,10 +38,13 @@ class CzujnikSnap():
       self.time_to_snap = time_to_snap
       self.mapa = None
       self.widget = None
+      self.options = Options()
+      self.driver = None
+      self.start()
 
    def readConfigFunction(self):
-       logging.debug("readConfig")
-       with open('config.json') as json_file:
+       logging.debug("readConfigFunction")
+       with open('czujnikSnapConfig.json') as json_file:
            urls = json.load(json_file)
            self.mapa = urls['mapa']
            self.widget=urls['widget']
@@ -48,26 +52,52 @@ class CzujnikSnap():
        logging.debug("widget:"+self.widget)
 
    def start(self):
-       print("czujnikSnap")
-       self.seleniumWork()
+       logging.debug("start: czujnikSnap")
+       self.readConfigFunction()
+       #opcje przeglądarki
+       self.options = Options()
+       self.options.add_argument("--no-sandbox")
+       self.options.add_argument("--start-fullscreen")
+       self.options.add_argument("--kiosk")
+       self.options.add_argument("--disable-application-cache")
+       logging.debug(f"options: {self.options}")
+       #driver przeglądarki
+       self.driver = webdriver.Chrome(options=self.options)
+       self.driver.set_window_size(1920,1316)
+       self.driver.set_script_timeout(30)
+       self.driver.set_page_load_timeout(30) # seconds
+       logging.debug(f"driver: {self.driver}")
+       self.seleniumJob()
 
-   def seleniumWork(self):
-       self.options = webdriver.ChromeOptions()
+   def seleniumJob(self):
+       logging.debug("seleniumJob - method")
        if self.loghami:
            self.options.headless=False
        else:
            self.options.headless=True
-       driver = webdriver.Chrome(options=self.options)
-       url='http://czujnikimiejskie.pl/public/kozienice/'
-       driver.get(url)
-       sleep(5)
-       screenshot = driver.save_screenshot('mapa.png')
-       driver.close()
+       #while True:
+       self.snapWidget()
+       self.snapMapa()
+       self.driver.quit()
 
-   def start(self):
-       print("czujnikSnap")
-       self.readConfigFunction()
-       self.seleniumWork()
+   def snapWidget(self):
+       logging.debug("snapWidget - robienie zdjęcia widgetu")
+       milli_sec = int(round(time.time()*1000))
+       now = datetime.now()
+       t = now.strftime("[%Y/%m/%d-%H:%M:%S]")
+       logging.debug(f"driver: {self.driver}")
+       self.driver.get(self.widget+str(milli_sec))
+       sleep(3)
+       screenshot = self.driver.save_screenshot('widget.png')
+
+   def snapMapa(self):
+       logging.debug("snapMapa - robienie zdjęcia widgetu")
+       milli_sec = int(round(time.time()*1000))
+       now = datetime.now()
+       t = now.strftime("[%Y/%m/%d-%H:%M:%S]")
+       self.driver.get(self.mapa+str(milli_sec))
+       sleep(3)
+       screenshot = self.driver.save_screenshot('mapa.png')
 
 def main():
     args=def_params()
