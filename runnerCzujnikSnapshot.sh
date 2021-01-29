f40c4766cef755a1acfc60683d31949455df1a7a
Hamilkar247 <adonnibal96@gmail.com>
2021-01-29 15:12:45 +0100

    [WO-99] problem uruchamiania programu - serwis uruchamiał go w /lib/systemd/system zamiast w naszych kochanym miejscu projektu z config.jsonw
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ runnerCzujnikSnapshot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Czujniki="$HOME""/Projects/czujnikSnapshot"
+PythonEnv="$HOME""/Projects/czujnikSnapshot/venv/bin/python"
+cd "$Czujniki"
+pwd
+"$PythonEnv" "$Czujniki"/czujnikSnapshot.py 
+
