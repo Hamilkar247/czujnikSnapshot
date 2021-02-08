90dfcb09c0fe1cd0c06fd10b5062970a49753be8
Hamilkar247 <adonnibal96@gmail.com>
2021-02-08 17:34:07 +0100

    [ISS-48] check_slideshow_work - pobieram zmieną odpowiadający za czas sprawdzania config.json
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- check_slideshow_work.sh
+++ check_slideshow_work.sh
@@ -2,18 +2,27 @@
 
 #zczytuje folder roboczy
 WD=$(sed 's/.*"workdirectory": "\(.*\)".*/\1/;t;d' config.json)
+timeToSleep=$(sed 's/.*"timeToCheckSlideshow": \(.*\),/\1/;t;d' config.json)
+if [[ "$timeToSleep" == "" ]]
+ then
+   timeToSleep=5
+   echo "ahjo nie ma zmiennej timeToSleep"
+   exit
+fi
 
 echo "$WD"
 cd "$WD" || return
 
+sleep 5 #czas by slideshow.py miał czas zczytać configa etc
 while true
 do
   echo "sprawdzam czy plik working_slideshow.txt istnieje"
   if [ -f "$WD""/working_slideshow.txt" ]
   then
     echo "usuwam plik working_slideshow.txt"
+    rm working_slideshow.txt
   else
     echo "nie znalazłem pliku"
   fi
-  sleep 3 # czas w sekundach
+  sleep "$timeToSleep" # czas w sekundach
 done
