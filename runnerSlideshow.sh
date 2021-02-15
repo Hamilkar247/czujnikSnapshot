8838ef70874d2d2768d2b97ea7484be7aae03f8f
Hamilkar247 <adonnibal96@gmail.com>
2021-02-15 14:30:25 +0100

    [ISS-59] dodanie parametru WorkingDirectory do serwisów
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- runnerSlideshow.sh
+++ runnerSlideshow.sh
@@ -5,6 +5,5 @@ Slideshow="$HOME""/Projects/czujnikSnapshot"
 echo "$Slideshow"
 PythonEnv="$HOME""/Projects/czujnikSnapshot/venv/bin/python"
 echo "$PythonEnv"
-cd "$Slideshow"
 pwd
 "$PythonEnv" "$Slideshow"/slideshow.py
