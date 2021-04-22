809a587f7f5cd1fdc43554ef163d99122581be80
Hamilkar247 <adonnibal96@gmail.com>
2021-04-22 16:07:32 +0200

    [#135] test_fixture.py - metoda sprzątająca, po wywołaniu już funkcji
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- test_fixture.py
+++ test_fixture.py
@@ -2,8 +2,14 @@ import pytest
 
 
 @pytest.fixture()
-def moj_pierwszy_fixture():
+def moj_pierwszy_fixture(request):
     print("Wykonuje fixture")
+
+    def na_zakonczenie():
+        print("Wykonuje po zakończonym teście \n")
+
+    # uwaga nazwa funkcji jako atrybut daj bez nawiasów!
+    request.addfinalizer(na_zakonczenie)
 
 
 @pytest.mark.usefixtures("moj_pierwszy_fixture")
@@ -17,4 +23,5 @@ def test_drugi():
 
 @pytest.mark.usefixtures("moj_pierwszy_fixture")
 def test_trzeci():
-    print("Wykonuje test trzeci\n")
+    print("Wykonuje test trzeci")
+    #assert 1 == 2
