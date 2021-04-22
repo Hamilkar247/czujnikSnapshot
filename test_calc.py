02e316c2b8089471f8c8fac696f8e211b2ff6b4c
Hamilkar247 <adonnibal96@gmail.com>
2021-04-22 13:15:34 +0200

    [#135] odkomentowanie testów
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- test_calc.py
+++ test_calc.py
@@ -1,12 +1,12 @@
 from base import BasicCalculator
 
 
-#def test_addition():
-#    object = BasicCalculator()
-#    object.provice_number(10)
-#    object.provide_operand('+')
-#    object.provice_number(5)
-#    assert object.show_result()[0] == 15
+def test_addition():
+    object = BasicCalculator()
+    object.provice_number(10)
+    object.provide_operand('+')
+    object.provice_number(5)
+    assert object.show_result()[0] == 15
 
 
 def test_subtraction():
