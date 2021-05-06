7f1da5e0271a3a9e177359bb4221411472542b5d
Hamilkar247 <adonnibal96@gmail.com>
2021-05-06 17:52:03 +0200

    [#148] wstepne otwieranie polaczenie ftp w testach - brak zapisu pliku
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ tests/test_ftp_get_picture.py
@@ -0,0 +1,53 @@
+import logging
+
+import pytest
+from without_wifi import FtpSlideshow
+import os
+
+
+def concatenate_list_data(list):
+    # print(list)
+    result = ''
+    for element in list:
+        if element != "":
+            result += "/" + element
+    return result
+
+
+@pytest.fixture()
+def chdir_root_folder():
+    path = os.getcwd()
+    print(path.split("/")[-1])
+    print(path.split("/")[-2])
+    if path.split("/")[-1] == "slideshow":
+        print("ok, jesteś w root folderze projektu")
+    if path.split("/")[-2] == "slideshow":
+        print("ok folder wyżej jest root folderem")
+        print(path.split("/")[0:-1])
+        root_folder = concatenate_list_data(path.split("/")[0:-1])
+        print(root_folder)
+        os.chdir(root_folder)
+
+
+def get_picture_via_sim800L(nazwa_zdjecia):
+    logging.debug("get_picture_via_sim800 - ahjo!")
+    try:
+        ftp_slideshow = FtpSlideshow(path="/dev/ttyUSB0", baudrate="115200")
+        return ftp_slideshow.get_file(extension="png", get_name_file=nazwa_zdjecia,
+                                      server_ip="37.48.70.196",
+                                      get_path_file="/hamilkar.cba.pl/Kozienice/",
+                                      nickname="hamilkar",
+                                      password="Hamilkar0")
+    except Exception as e:
+        return False
+    return True
+
+
+@pytest.mark.usefixtures("chdir_root_folder")
+def test_ftp_get_picture():
+    assert get_picture_via_sim800L()
+
+
+@pytest.mark.usefixtures("chdir_root_folder")
+def test_ftp_get_picture():
+    assert get_picture_via_sim800L()
--- without_wifi/withoutwifi.py
+++ without_wifi/withoutwifi.py
@@ -40,6 +40,19 @@ class FtpSlideshow:
             print("Wystąpił błąd przy próbie otwarcia portu Ftplideshow - możliwe że inny program używa już podanego portu!")
             traceback.print_exc()
 
+    def get_file(self, APN="internet",extension="png", get_name_file="widget.png", server_ip="37.48.70,196", port=21, mode=0,
+                 get_path_file="/", nickname="qaz", password="zxc"):
+        logging.debug("get_file <-- FtpSlideshow ")
+        try:
+            self.ftp.request_ftp.getFile(APN=APN, server_ip=server_ip, port=21, mode=0,
+                                         get_name_file=get_name_file, get_path_file=get_path_file
+                                         , nickname=nickname, password=password)
+        except Exception as e:
+            print("Niestety - nie udało się wysłać wiadomości na serwer")
+            print(f"{e}")
+            return False
+        return True
+
     def post_file(self, APN="internet", server_ip="37.48.70.196", port=21, mode=0,
                   put_name_file="hami.json", get_name_file="hami.json",
                   put_path_file="/hamilkar.cba.pl/myhero/",
